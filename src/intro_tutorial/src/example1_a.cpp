f2a27e61c11f305237a5455a3e5b28b5f83d0a17
Gionata Grotto <g.gionata99@gmail.com>
2022-11-14 22:46:05 +0100

    Comments

--- src/intro_tutorial/src/example1_a.cpp
+++ src/intro_tutorial/src/example1_a.cpp
@@ -3,20 +3,40 @@
 #include <sstream>
 
 int main(int argc, char **argv) {
+    // initialize ros to use parameters
     ros::init(argc, argv, "example1_a");
+
+    // initialize node
     ros::NodeHandle n;
-    ros::Publisher chatter_pub =
-    n.advertise<std_msgs::String>("message", 1000);
+
+    // generate publisher to publish messages
+    // first parameter define the name of the publisher
+    // second parameter define the dimension of the buffer
+    ros::Publisher chatter_pub = n.advertise<std_msgs::String>("message", 1000);
+
+    // frequency of loop
     ros::Rate loop_rate(10);
+
     while (ros::ok()) {
+        // create a standard message
         std_msgs::String msg;
+
+        // create a string
         std::stringstream ss;
         ss << " Hello World! ";
+
+        // assign the string to the message
         msg.data = ss.str();
+
+        // print our message to check
         ROS_INFO("%s", msg.data.c_str());
+
+        // publish the message in the publisher object
         chatter_pub.publish(msg);
         ros::spinOnce();
+
+        // to have the frequency specified in loop_rate
         loop_rate.sleep();
     }
-    return 0;
+    return 0; 
 }
--- src/intro_tutorial/src/example1_b.cpp
+++ src/intro_tutorial/src/example1_b.cpp
@@ -1,6 +1,7 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
+// callback function
 void chatterCallback(const std_msgs::String::ConstPtr& msg) {
     ROS_INFO("I heard: [%s]", msg->data.c_str());
 }
@@ -8,8 +9,10 @@ void chatterCallback(const std_msgs::String::ConstPtr& msg) {
 int main(int argc, char **argv) {
     ros::init(argc, argv, "example1_b");
     ros::NodeHandle n;
-    ros::Subscriber sub = n.subscribe("message",
-    1000, chatterCallback);
+
+    // subscriber to the publisher
+    ros::Subscriber sub = n.subscribe("message", 1000, chatterCallback);
+    
     ros::spin();
     return 0;
 }
